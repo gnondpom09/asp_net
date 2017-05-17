1079f82dad8b033701be4b78ecbcc844195c3c6f
Laurent botella <agence@paus-web.fr>
2017-05-17 21:45:27 +0200

    ajout page biographie et liens effectifs

--- botella/App_Start/RouteConfig.cs
+++ botella/App_Start/RouteConfig.cs
@@ -9,10 +9,17 @@ namespace botella
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            // page Accueil
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+            // page Biography
+            routes.MapRoute(
+                name: "Biograhy",
+                url: "{controller}/{action}",
+                defaults: new { controller = "Bio", action = "Biographie" }
             );
         }
     }
